07186c19857e4eb59d1ba06eadcae0facc0daad3
Michael Chow <mc_al_github@fastmail.com>
2021-08-28 15:17:42 -0400

    fix(tests): fix test that only is correct for sqlalchemy 1.4

--- siuba/tests/test_verb_show_query.py
+++ siuba/tests/test_verb_show_query.py
@@ -40,7 +40,7 @@ def test_show_query_complex_simplify(df_wide):
     res = q >> show_query(return_query = False, simplify=True)
 
     assert rename_source(str(res), df_wide) == """\
-SELECT a, avg(a) OVER () AS b 
+SELECT x, y, z, a, avg(a) OVER () AS b 
 FROM (SELECT *, avg(x) OVER () AS a 
 FROM SRC_TBL) AS anon_1"""
 
